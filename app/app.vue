7c38af7b8a9c0b742bd585fe1faeaf4a06cc05f9
Navaphan Singkaew <copnavaphan@gmail.com>
2026-04-19 02:28:05 +0700

    ✨feat: update font english & thai

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,13 @@
+<script setup lang="ts">
+const { locale } = useKbixLocale()
+
+useHead(() => ({
+  htmlAttrs: {
+    lang: locale.value,
+  },
+}))
+</script>
+
 <template>
   <UApp>
     <NuxtRouteAnnouncer />
